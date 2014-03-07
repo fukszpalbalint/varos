007f676d47179fc69cb54f3aa90cf079f035bd60
fukszpalbalint <fukszpalbalint@gmail.com>
2014-03-07 15:48:29 +0100

    Update 5x2-pali.js

--- mezok/5x2-pali.js
+++ mezok/5x2-pali.js
@@ -4,6 +4,7 @@ mezo({
   nev: 'Pali Házikó',
   leiras: 'kicsi házikó'
   ralepeskor: function() {
+  kiir ('Pali hzára léptél és fájt neki!')
   nev = kerdez("Mi a neved?");
   udvozlet = "Szia " + nev;
   kiir(udvozlet);
